e5aa67e3b63e341e15462c51984c5f68bb1c327c
Oleksandr Bereza <oba@simcorp.com>
2020-08-18 01:55:43 +0200

    Test to verify that MemoryCalc is instantiated with zero as accumulated sum

--- test/MemoryCalcTest.cs
+++ test/MemoryCalcTest.cs
@@ -1,8 +1,20 @@
 ﻿using NUnit.Framework;
+using Shouldly;
 
 namespace TddTrainingDay01.Test {
     [TestFixture]
     public class MemoryCalcTest {
+        [Test]
+        public void GetTotal_WhenClassInstantiated_ReturnsZero() {
+            var sut = MakeMemoryCalc();
 
+            var act = sut.GetTotal();
+
+            act.ShouldBe(0);
+        }
+
+        private static MemoryCalc MakeMemoryCalc() {
+            return new MemoryCalc();
+        }
     }
 }
