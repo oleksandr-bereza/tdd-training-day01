bf1870f45a2c3129ce960585a6f1957037ebed9d
Oleksandr Bereza <oba@simcorp.com>
2020-08-18 00:19:25 +0200

    Tests to cover first exit point in MyCalc

--- test/MyCalcTest.cs
+++ test/MyCalcTest.cs
@@ -1,8 +1,25 @@
 ﻿using NUnit.Framework;
+using Shouldly;
 
 namespace TddTrainingDay01.Test {
     [TestFixture]
     public class MyCalcTest {
+        [Test]
+        public void Add_WhenFirstParameterIsNonPositive_ReturnsMinusOne() {
+            var sut = new MyCalc();
 
+            var act = sut.Add(0, 1);
+
+            act.ShouldBe(-1);
+        }
+
+        [Test]
+        public void Add_WhenSecondParameterIsNonPositive_ReturnsMinusOne() {
+            var sut = new MyCalc();
+
+            var act = sut.Add(1, 0);
+
+            act.ShouldBe(-1);
+        }
     }
 }
