5b19d2f86bf41e9ca0d2f5b5dd584b0b4ff4d398
Oleksandr Bereza <oba@simcorp.com>
2020-08-18 02:30:33 +0200

    Test to verify reset works as expected

--- test/MemoryCalcTest.cs
+++ test/MemoryCalcTest.cs
@@ -26,6 +26,20 @@ namespace TddTrainingDay01.Test {
             act.ShouldBe(expectedValue);
         }
 
+        [TestCase(1, 2)]
+        [TestCase(-1, -2)]
+        [TestCase(123, -123)]
+        public void GetTotal_WhenResetIsCalledAfterCallingAdd_ReturnsZero(int a, int b) {
+            var sut = MakeMemoryCalc();
+            sut.Add(a);
+            sut.Add(b);
+            sut.Reset();
+
+            var act = sut.GetTotal();
+
+            act.ShouldBe(0);
+        }
+
         private static MemoryCalc MakeMemoryCalc() {
             return new MemoryCalc();
         }
